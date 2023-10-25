8b157e0f3e95bf293e8c090b1dcba37717cfeb1c
Roman Vassilchenko <roman.vassilchenko.work@gmail.com>
2023-10-25 17:52:18 +0600

    Base operations in moodle api:   Get user information   Get user's courses   Get upcoming deadlines
    Do not forget to add your apiToken for testing

--- cmd/ReMoodle/main.go
+++ cmd/ReMoodle/main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ocean-planet/ReMoodle/internal/app/moodle"
+	"log"
 )
 
 func main() {
-	//ctx := context.Background()
-	// Инициализация репозитория и сервиса
-	repo := moodle.NewMoodleRepository("https://moodle.astanait.edu.kz/webservice/rest/server.php?wstoken=")
-	service := moodle.NewMoodleService(repo)
+	apiToken := ""
 
-	service.GetDeadlines("")
+	apiLink := "https://moodle.astanait.edu.kz/webservice/rest/server.php?wstoken=" + apiToken
+	repo := moodle.NewMoodleRepository(apiLink)
 
-	// Настройка маршрутов HTTP
-	//http.HandleFunc("/user", moodle.GetUserHandler)
-	//http.HandleFunc("/courses", moodle.GetUserCoursesHandler)
-	//http.HandleFunc("/deadlines", moodle.GetDeadlinesHandler)
+	// Replace "yourtoken" with your actual Moodle API token.
 
-	// Запуск сервера
-	//port := 8080
-	//fmt.Printf("Server is running on port %d\n", port)
-	//http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// Example 1: Get user information
+	userInfo, err := repo.GetUserInfo(apiToken)
+	if err != nil {
+		log.Fatalf("Error getting user info: %v", err)
+	}
+	fmt.Printf("User Info:\nBarcode: %s\nFull Name: %s\nUser ID: %s\n", userInfo.Barcode, userInfo.FullName, userInfo.UserID)
+
+	// Example 2: Get user's courses
+	courses, err := repo.GetUserAllCourses(apiToken)
+	if err != nil {
+		log.Fatalf("Error getting user courses: %v", err)
+	}
+	fmt.Println("\nUser Courses:")
+	for _, course := range courses {
+		fmt.Printf("Course Name: %s\nEnrolled User Count: %d\n", course.Name, course.EnrolledUserCount)
+	}
+
+	// Example 3: Get upcoming deadlines
+	deadlines, err := repo.GetDeadlines(apiToken)
+	if err != nil {
+		log.Fatalf("Error getting deadlines: %v", err)
+	}
+	fmt.Println("\nUpcoming Deadlines:")
+	for _, deadline := range deadlines {
+		fmt.Printf("Course Name: %s\nDeadline Name: %s\nRemaining Time: %d\n", deadline.CourseName, deadline.DeadlineName, deadline.Remaining)
+	}
+
 }
--- internal/app/moodle/repository.go
+++ internal/app/moodle/repository.go
@@ -1,5 +1,13 @@
 package moodle
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
 type MoodleRepository struct {
 	MoodleAPILink string
 }
@@ -11,16 +19,145 @@ func NewMoodleRepository(apiLink string) *MoodleRepository {
 }
 
 func (r *MoodleRepository) GetUserInfo(token string) (*User, error) {
-	// Реализация получения информации о пользователе из Moodle API
-	return nil, nil
+	url := fmt.Sprintf("%s&wstoken=%s&wsfunction=core_webservice_get_site_info&moodlewsrestformat=json", r.MoodleAPILink, token)
+	fmt.Println("URL:", url) // Debugging: Print the URL
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("Error closing response body: %v\n", err)
+		}
+	}(resp.Body)
+
+	fmt.Println("Response Status Code:", resp.Status)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("moodle API request failed with status code %d", resp.StatusCode)
+	}
+
+	var response map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		fmt.Println("Error decoding JSON response:", err)
+		return nil, err
+	}
+
+	user := &User{
+		Barcode:  response["username"].(string),
+		FullName: response["fullname"].(string),
+		UserID:   strconv.FormatFloat(response["userid"].(float64), 'f', -1, 64),
+	}
+
+	return user, nil
 }
 
 func (r *MoodleRepository) GetUserAllCourses(token string) ([]Course, error) {
-	// Реализация получения всех курсов пользователя из Moodle API
-	return nil, nil
+	user, err := r.GetUserInfo(token)
+	if err != nil {
+		return nil, err
+	}
+	userID := user.UserID
+
+	url := fmt.Sprintf("%s&wstoken=%s&wsfunction=core_enrol_get_users_courses&moodlewsrestformat=json&userid=%s", r.MoodleAPILink, token, userID)
+	fmt.Println("URL:", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("Error closing response body: %v\n", err)
+		}
+	}(resp.Body)
+
+	fmt.Println("Response Status Code:", resp.Status)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("moodle API request failed with status code %d", resp.StatusCode)
+	}
+
+	var response []map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		fmt.Println("Error decoding JSON response:", err)
+		return nil, err
+	}
+
+	courses := make([]Course, len(response))
+
+	for i, courseData := range response {
+
+		course := Course{
+			ID:                int(courseData["id"].(float64)),
+			Name:              courseData["displayname"].(string),
+			EnrolledUserCount: int(courseData["enrolledusercount"].(float64)),
+			Category:          strconv.FormatFloat(courseData["category"].(float64), 'f', -1, 64),
+			// You may need to handle other fields like 'completed', 'start_date', and 'end_date' based on your response structure.
+		}
+		courses[i] = course
+	}
+
+	return courses, nil
 }
 
 func (r *MoodleRepository) GetDeadlines(token string) ([]Deadline, error) {
-	// Реализация получения сроков из Moodle API
-	return nil, nil
+	url := fmt.Sprintf("%s&wstoken=%s&wsfunction=core_calendar_get_calendar_upcoming_view&moodlewsrestformat=json", r.MoodleAPILink, token)
+	fmt.Println("URL:", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("Error closing response body: %v\n", err)
+		}
+	}(resp.Body)
+
+	fmt.Println("Response Status Code:", resp.Status)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("moodle API request failed with status code %d", resp.StatusCode)
+	}
+
+	var response map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		fmt.Println("Error decoding JSON response:", err)
+		return nil, err
+	}
+
+	events, ok := response["events"].([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("invalid response format for deadlines")
+	}
+
+	deadlines := make([]Deadline, len(events))
+
+	for i, event := range events {
+		eventData, ok := event.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("invalid event data format")
+		}
+
+		deadline := Deadline{
+			ID:           int(eventData["id"].(float64)),
+			CourseName:   eventData["course"].(map[string]interface{})["shortname"].(string),
+			DeadlineName: eventData["name"].(string),
+			Remaining:    int64(eventData["timestart"].(float64)),
+		}
+		deadlines[i] = deadline
+	}
+
+	return deadlines, nil
 }
