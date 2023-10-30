536c248183a10de76e9e6d202fc26f70aee7c17f
Alex Sehl <widesehl@gmail.com>
2023-10-31 04:31:48 +0600

    Add mvp whoami command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ocean-planet/ReMoodle/internal/app/commands/command"
 	"github.com/ocean-planet/ReMoodle/internal/app/commands/help"
 	"github.com/ocean-planet/ReMoodle/internal/app/commands/login"
+	"github.com/ocean-planet/ReMoodle/internal/app/commands/whoami"
 	"github.com/ocean-planet/ReMoodle/internal/app/core"
 )
 
@@ -16,6 +17,7 @@ func main() {
 
 	commandRepository.RegisterCommand("help", &help.HelpCommand{CommandService: commandService})
 	commandRepository.RegisterCommand("login", &login.LoginCommand{CommandService: commandService})
+	commandRepository.RegisterCommand("whoami", &whoami.WhoamiCommand{CommandService: commandService})
 
 	if err := myApp.Run(os.Args[1:]); err != nil {
 		os.Exit(1)
---
+++ internal/app/commands/whoami/whoami.go
@@ -0,0 +1,37 @@
+package whoami
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ocean-planet/ReMoodle/internal/app/commands/command"
+	"github.com/ocean-planet/ReMoodle/internal/app/core"
+	"github.com/ocean-planet/ReMoodle/internal/app/moodle"
+)
+
+type WhoamiCommand struct {
+	CommandService command.CommandService
+}
+
+func (h *WhoamiCommand) Execute(_ []string) error {
+	token, err := core.LoadToken()
+
+	if err != nil {
+		return err
+	}
+
+	apiLink := "https://moodle.astanait.edu.kz/webservice/rest/server.php?wstoken=" + token
+	repo := moodle.NewMoodleRepository(apiLink)
+
+	userInfo, err := repo.GetUserInfo(token)
+	if err != nil {
+		log.Fatalf("Error getting user info: %v", err)
+	}
+	fmt.Printf("User Info:\nBarcode: %s\nFull Name: %s\nUser ID: %s\n", userInfo.Barcode, userInfo.FullName, userInfo.UserID)
+
+	return nil
+}
+
+func (h *WhoamiCommand) Description() string {
+	return "Show current user info"
+}
