994fbcbd62be9c6c71d32158e09263374ffbc752
Yelnur <elnur.kazakhstan@gmail.com>
2023-11-01 21:45:10 +0600

    Add courses command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/ocean-planet/ReMoodle/internal/app/commands/command"
+	"github.com/ocean-planet/ReMoodle/internal/app/commands/courses"
 	"github.com/ocean-planet/ReMoodle/internal/app/commands/deadlines"
 	"github.com/ocean-planet/ReMoodle/internal/app/commands/help"
 	"github.com/ocean-planet/ReMoodle/internal/app/commands/login"
@@ -20,6 +21,7 @@ func main() {
 	commandRepository.RegisterCommand("login", &login.LoginCommand{CommandService: commandService})
 	commandRepository.RegisterCommand("whoami", &whoami.WhoamiCommand{CommandService: commandService})
 	commandRepository.RegisterCommand("deadlines", &deadlines.DeadlineCommand{CommandService: commandService})
+	commandRepository.RegisterCommand("courses", &courses.CoursesCommand{CommandService: commandService})
 
 	if err := myApp.Run(os.Args[1:]); err != nil {
 		os.Exit(1)
--- internal/app/commands/courses/courses.go
+++ internal/app/commands/courses/courses.go
@@ -1,1 +1,56 @@
 package courses
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/ocean-planet/ReMoodle/internal/app/commands/command"
+	"github.com/ocean-planet/ReMoodle/internal/app/core"
+	"github.com/ocean-planet/ReMoodle/internal/app/moodle"
+)
+
+type CoursesCommand struct {
+	CommandService command.CommandService
+}
+
+// Description implements command.Command.
+func (*CoursesCommand) Description() string {
+	return "shows current courses"
+}
+
+func (c *CoursesCommand) Execute(args []string) error {
+	token, tokenErr := core.LoadToken()
+
+	if tokenErr != nil {
+		return tokenErr
+	}
+
+	moodleRepository := moodle.NewMoodleRepository("https://moodle.astanait.edu.kz/webservice/rest/server.php")
+	moodleService := moodle.NewMoodleService(moodleRepository)
+
+	courses, err := moodleService.GetUserAllCourses(token)
+
+	if err != nil {
+		fmt.Println("Moodle is currently unavailable, try again later")
+		return err
+	}
+
+	if len(courses) < 1 {
+		fmt.Println("Courses wasn't found")
+		return nil
+	}
+
+	fmt.Println("Current courses:")
+
+	currentUnixTime := time.Now().Unix()
+	// relativeCourses := make([]moodle.Course, 0)
+
+	for _, course := range courses {
+		if course.EndDate > currentUnixTime {
+			// relativeCourses = append(relativeCourses, course)
+			fmt.Println("> 🆔 " + fmt.Sprint(course.ID) + " | 📚 " + course.Name)
+		}
+	}
+
+	return nil
+}
